dd7f6aecefaa1119d803000ff3705bc91f44a07d
Yoan Villeneuve <yo10_snow@hotmail.com>
2019-03-02 01:40:18 -0500

    Ajout au script NPC_1 et nouveau dialog

--- Assets/Script/NPC_1.cs
+++ Assets/Script/NPC_1.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 /*Fille qui trip sur les gros muscle*/
 public class NPC_1 : MonoBehaviour
 {
-    private static int BaseStrengh = 5;
-    private static int BaseCardio = 2;
-    private static int BaseDance = 2;
-    private static int BaseIntel = 1;
+    private int BaseStrengh = 5;
+    private int BaseCardio = 2;
+    private int BaseDance = 2;
+    private int BaseIntel = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +23,31 @@ public class NPC_1 : MonoBehaviour
         
     }
 
-    public void coochieGetter(int week, int intel, int cardio, int strenght, int dance)
+    /*Methode pour savoir si les statistique requise sont atteinte.**
+     *Retourne le nom du dialog à utiliser**/
+    public string coochieGetter(int week, int intel, int cardio, int strengh, int dance)
     {
-        //trouver + grande valeur
-        int[] tmp = new int[4];
+        //updateBaseStats(week);
 
+        if (intel >= BaseIntel)
+            if (cardio >= BaseCardio)
+                if (strengh >= BaseStrengh)
+                    if (dance >= BaseDance)
+                        return "Dialog1";
+
+        return "Dialog2";
     }
 
+    /*
+    public void updateBaseStats()
+    {
+        //Adapter les valeurs par rapport à la semaine
+        
+        double constWeek = week * 0.3;
+        int intelNeeded = (int)Math.Round(intel + (intel * constWeek));
+        
+    }
+    //*/
 
 
 
