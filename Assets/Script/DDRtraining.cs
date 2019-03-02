700dc3d3b14945bf25aaa92af408c5fce448373a
Yoan Villeneuve <yo10_snow@hotmail.com>
2019-03-02 18:21:42 -0500

    Last branch push

--- Assets/Script/DDRtraining.cs
+++ Assets/Script/DDRtraining.cs
@@ -6,27 +6,41 @@ public class DDRtraining : MonoBehaviour
 {
     //Variables
     private Vector3 move;
-
     public GameObject DDRleft;
-    double m_XAxis, m_YAxis;
+    double m_YAxis;
+    
+    private static double statArrowY = -135;
 
     // Start is called before the first frame update
     void Start()
     {
         //Fetch the RectTransform from the GameObject
-
         //DDRleft = GetComponent<RectTransform>();
 
         //Initiate the x and y positions
         //move = DDRleft.position;
         move = DDRleft.GetComponent<RectTransform>().position;
+       // move.Set((float)-112.5, 237, 0);
+        //DDRleft.GetComponent<RectTransform>()
+            //.SetPositionAndRotation(move, DDRleft.GetComponent<RectTransform>().rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_YAxis = move.y - 1.0;
+        m_YAxis = move.y - 2.0;
         move.Set(move.x, (float)m_YAxis, move.z);
-        DDRleft.GetComponent<RectTransform>().SetPositionAndRotation(move, DDRleft.GetComponent<RectTransform>().rotation);
+        DDRleft.GetComponent<RectTransform>()
+            .SetPositionAndRotation(move, DDRleft.GetComponent<RectTransform>().rotation);
+
+        if ((statArrowY + 40.0) >= m_YAxis)
+            if(m_YAxis >= (statArrowY + 40.0))
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                Debug.Log(m_YAxis);
+                this.enabled = false;
+            }
+        }
     }
 }
