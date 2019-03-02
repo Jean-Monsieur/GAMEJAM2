3d378d491fe1a207437633bbccd6c72c26cffd7e
mpoirel <mathieu_poirel@hotmail.com>
2019-03-02 02:04:35 -0500

    Travail sur le bench pressed

--- Assets/Script/BenchPressed.cs
+++ Assets/Script/BenchPressed.cs
@@ -9,7 +9,7 @@ public class BenchPressed : MonoBehaviour
     private float StartYPos;
     private float yPos;
     int maxLevel;
-    bool win;
+    bool Gamefinished;
 
     [SerializeField]
     GameObject WinScreen;
@@ -18,58 +18,92 @@ public class BenchPressed : MonoBehaviour
     GameObject LooseScreen;
 
     [SerializeField]
+    GameObject readyScreen;
+
+    [SerializeField]
     GameObject GameMaster;
 
     [SerializeField]
     GameObject player;
 
+    [SerializeField]
+    Text startTimer;
+
+
+    int difficulty;
     int Strenght;
     int hit;
     int hitPosition;
+    int releaseRatio;
+    bool start ;
+    bool timerFinished;
 
     // Start is called before the first frame update
     void Start()
     {
-        win = false;
+        Gamefinished = false;
         StartYPos = this.transform.position.y;
         yPos = StartYPos;
         maxLevel = 11;
         hitPosition = 1;
         hit = 0;
-        //StartCoroutine(Timer());
+        releaseRatio = 0;
+        StartCoroutine(Timer());
+        timerFinished = false;
+        start = false;
+        difficulty = 75;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Hit") && yPos <= maxLevel)
+
+
+        if (Input.GetButtonDown("Hit") && Gamefinished == false)
         {
-            hit += 1;
-            //yPos += 2;
-            //this.transform.position = (new Vector3(this.transform.position.x, yPos, this.transform.position.z));
-            if (hit == 2)
+
+            
+            if (timerFinished == true)
             {
-                hitPosition += 1;
-                player.GetComponent<Image>().sprite =  Resources.Load<Sprite>("Sprites/Benchpress/BenchAnimation_" + hitPosition.ToString());
-                hit = 0;
+                hit += 1;
+                //yPos += 2;
+                //this.transform.position = (new Vector3(this.transform.position.x, yPos, this.transform.position.z));
+                if (hit == 2)
+                {
+                    hitPosition += 1;
+                    player.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Benchpress/BenchAnimation_" + hitPosition.ToString());
+                    hit = 0;
+                }
             }
-            
 
+            if (start == false)
+            {
+                start = true;
+                readyScreen.SetActive(false);  
+                StartCoroutine(StartGame());
+            }
         }
 
-        if(yPos > StartYPos && yPos <= maxLevel)
-        {
-            yPos -= 0.1f;
-            this.transform.position = (new Vector3(this.transform.position.x, yPos, this.transform.position.z));
+        releaseRatio++;
 
+        if (releaseRatio >= difficulty && hitPosition != 1 && Gamefinished == false && start == true)
+        {
+            //yPos -= 0.1f;
+            //this.transform.position = (new Vector3(this.transform.position.x, yPos, this.transform.position.z));
+            hitPosition -= 1;
+            player.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Benchpress/BenchAnimation_" + hitPosition.ToString());
+            releaseRatio = 0;
         }
 
 
-        if (hit == maxLevel)
+        if (hitPosition == maxLevel)
         {
-            GameMaster.GetComponent<GameMaster>().setStrenght(5);
+            // GameMaster.GetComponent<GameMaster>().setStrenght(5);
             WinScreen.SetActive(true);
-            
+            Gamefinished = true;
+
+
         }
 
 
@@ -78,11 +112,33 @@ public class BenchPressed : MonoBehaviour
 
     IEnumerator Timer()
     {
-        yield return new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(8.0f);
 
-        if(win == false)
+        if (Gamefinished == false)
+        {
             LooseScreen.SetActive(true);
+            Gamefinished = true;
+
+        }
+           
     }
 
 
+    IEnumerator StartGame()
+    {
+        for (int i = 3; i> 0; i--)
+        {
+            startTimer.text = i.ToString();
+            yield return new WaitForSeconds(1.0f);
+            if (i == 1)
+            {
+                startTimer.text = "";
+                timerFinished = true;
+            }
+
+        }
+        
+
+
+    }
 }
