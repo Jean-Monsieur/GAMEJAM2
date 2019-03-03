bffd9f8ac579535a8f61584d95d7c6684f8789aa
kyrhaTV <kyrha1719@gmail.com>
2019-03-02 19:17:30 -0500

    Uploaded SFXDirectory & applied sound on click

---
+++ Assets/Script/AudioPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip clickSound;
+
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Click()
+    {
+        audioSource.PlayOneShot(clickSound, 2.5F);
+    }
+
+}
