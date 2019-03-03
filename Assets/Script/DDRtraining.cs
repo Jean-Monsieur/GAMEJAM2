c70184038ae5db75d8a9931452ae6a95038eb837
Yoan Villeneuve <yo10_snow@hotmail.com>
2019-03-02 22:24:46 -0500

    modif DDR

--- Assets/Script/DDRtraining.cs
+++ Assets/Script/DDRtraining.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DDRtraining : MonoBehaviour
 {
     //Variables
     //public Text text;
     private Vector3 move;
-    public GameObject DDRleft;
+    public GameObject DDRArrow;
+    private RectTransform Arrow;
+    private RectTransform Arrows;
 
     double m_YAxis;
     private int points = 0;
@@ -15,16 +18,18 @@ public class DDRtraining : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        move = DDRleft.GetComponent<RectTransform>().position;
+        Arrow = DDRArrow.GetComponent<RectTransform>();
+        move = Arrow.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         m_YAxis = move.y - 2.0;
         move.Set(move.x, (float)m_YAxis, move.z);
-        DDRleft.GetComponent<RectTransform>()
-            .SetPositionAndRotation(move, DDRleft.GetComponent<RectTransform>().rotation);
+        Arrow.SetPositionAndRotation(move, Arrow.rotation);
         
     }
 
