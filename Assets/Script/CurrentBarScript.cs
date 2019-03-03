695454fb8f0f33be327263e93d9f16874f7101eb
kyrhaTV <kyrha1719@gmail.com>
2019-03-02 22:05:16 -0500

    ROLL du bar

---
+++ Assets/Script/CurrentBarScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CurrentBarScript : MonoBehaviour
+{
+
+    string bar;
+    [SerializeField]
+    GameObject LogoVM, LogoDAG, LogoBaru, LogoBiss;
+
+    bool toggle = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         toggle = false;
+        switch (GetBar())
+        {
+            case 1:
+                bar = "VM";
+                LogoVM.SetActive(!toggle);
+                break;
+            case 2:
+                bar = "DAG";
+                LogoDAG.SetActive(!toggle);
+                break;
+            case 3:
+                bar = "BARU";
+                LogoBaru.SetActive(!toggle);
+                break;
+            case 4:
+                bar = "BISS";
+                LogoBiss.SetActive(!toggle);
+                break;
+        }
+        Debug.Log(GetBar().ToString());
+        Debug.Log(bar);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public int GetBar()
+    {
+
+        int bar = Random.Range(1, 5);
+
+        return bar;
+    }
+}
